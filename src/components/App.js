c05fde9dbab273264e945fd480d71222d723892b
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2023-12-03 23:58:04 -0300

    adicionado os componentes do step 1 dentro da section plan custom

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
 import '../css/App.css';
-import StepsBox from './Steps';
+import Step1 from './Step1';
+import StepsBox from './StepsBox';
+import { useState, useContext, createContext } from 'react';
+
+const StepContext = createContext();
 
 export default function App() {
+  const [step, setStep] = useState(1);
+
   const stepNames = ["YOUR INFO", "SELECT PLAN", "ADD-ONS", "SUMMARY"];
 
   return (
-    <main className="main-sec">
-      <section id='step-guide-container'>
-        {stepNames.map((stepName, index) => <StepsBox number={index + 1} name={stepName}/>)}
-      </section>
-      <section id='plan-custom'></section>
-    </main>
+    <StepContext.Provider value={{step, setStep}}>
+      <main className="main-sec">
+        <section id='step-guide-container'>
+          {stepNames.map((stepName, index) => <StepsBox number={index + 1} name={stepName} key={index + 1}/>)}
+        </section>
+        <section id='plan-custom'>
+          <Step1/>
+        </section>
+      </main>
+    </StepContext.Provider>
   );
 };
+
+export { StepContext }
