6fdb7d2f22c73e9a10b1104fe1edb00131e72b66
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2023-12-10 11:48:52 -0300

    personalização do valor e da periodicidade dos serviços adicionais

--- src/components/Step4/AddonsBill.js
+++ src/components/Step4/AddonsBill.js
@@ -4,53 +4,62 @@ import {
   OnlineServiceContext,
   LargerStorageContext,
   CustomizableProfileContext,
+  PeriodicContext,
 } from "../App.js";
 
-function OnlineServiceBill() {
+function OnlineServiceBill({ periodicity }) {
   const onlineService = useContext(OnlineServiceContext);
 
   if (onlineService.onlineServiceState) {
     return (
       <div className="addons-bill-info">
         <p className="bill-name">Online Service</p>
-        <p className="addons-price">+10/yr</p>
+        <p className="addons-price">
+          {periodicity === "Monthly" ? "+$1/mo" : "+$10/yr"}
+        </p>
       </div>
     );
   }
 }
 
-function LarerStorageBill() {
+function LarerStorageBill({ periodicity }) {
   const largerStorage = useContext(LargerStorageContext);
 
   if (largerStorage.largerStorageState) {
     return (
       <div className="addons-bill-info">
         <p className="bill-name">Larger Storage</p>
-        <p className="addons-price">+20/yr</p>
+        <p className="addons-price">
+          {periodicity === "Monthly" ? "+$2/mo" : "+$20/yr"}
+        </p>
       </div>
     );
   }
 }
 
-function CustomizableProfileBill() {
+function CustomizableProfileBill({ periodicity }) {
   const customizableProfile = useContext(CustomizableProfileContext);
 
   if (customizableProfile.customizableProfileState) {
     return (
       <div className="addons-bill-info">
         <p className="bill-name">Customizable Profile</p>
-        <p className="addons-price">+20/yr</p>
+        <p className="addons-price">
+          {periodicity === "Monthly" ? "+$2/mo" : "+$20/yr"}
+        </p>
       </div>
     );
   }
 }
 
 export default function AddonsBill() {
+  const periodic = useContext(PeriodicContext);
+
   return (
     <>
-      <OnlineServiceBill />
-      <LarerStorageBill />
-      <CustomizableProfileBill />
+      <OnlineServiceBill periodicity={periodic.periodicState} />
+      <LarerStorageBill periodicity={periodic.periodicState} />
+      <CustomizableProfileBill periodicity={periodic.periodicState} />
     </>
   );
 }
