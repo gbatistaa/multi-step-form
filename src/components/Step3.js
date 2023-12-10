444a0d528de8a824b5e658ca6e527d6594046743
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2023-12-10 10:50:35 -0300

    adicionada adição de estilização circunstancial acessando e alterando o estado dos inputs

--- src/components/Step3.js
+++ src/components/Step3.js
@@ -1,30 +1,59 @@
 import { useContext, useState } from "react";
-import { StepContext, PeriodicContext } from "./App.js";
+import {
+  StepContext,
+  PeriodicContext,
+  OnlineServiceContext,
+  LargerStorageContext,
+  CustomizableProfileContext,
+} from "./App.js";
 import "../css/Step3.css";
 
 export default function Step3() {
+  // Contexts:
+
   const planStep = useContext(StepContext);
   const planPeriod = useContext(PeriodicContext);
+  const onlineService = useContext(OnlineServiceContext);
+  const largerStorage = useContext(LargerStorageContext);
+  const customizableProfile = useContext(CustomizableProfileContext);
 
-  const [onlineServiceState, setOnlineServiceState] = useState(true);
-  const [largerStorageState, setLargeStorageState] = useState(false);
-  const [customizableProfileState, setCustomizableProfileState] =
-    useState(false);
+  const handleInputChange = (event) => {
+    const input = event.target;
+
+    switch (input.name) {
+      case "online-service":
+        onlineService.setOnlineServiceState(input.checked);
+        break;
+
+      case "larger-storage":
+        largerStorage.setLargeStorageState(input.checked);
+        break;
+
+      case "customizable-profile":
+        customizableProfile.setCustomizableProfileState(input.checked);
+        break;
+
+      default:
+        console.log(undefined);
+        break;
+    }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
   };
+
   return (
     <>
       <h1 className="main-title">Pick add-ons</h1>
       <p className="step-description">
-        Add-ons help enhance your gaming experience
+        Add-ons help enhance your gaming experience.
       </p>
-      <form onSubmit={(e) => handleSubmit(e)}>
+      <form onSubmit={(e) => handleSubmit(e)} className="form-3">
         <div className="selection-div">
           <label
             className={`select-addons ${
-              onlineServiceState ? "selected" : "not-selected"
+              onlineService.onlineServiceState ? "selected" : "not-selected"
             }`}
             htmlFor="online-service"
           >
@@ -33,10 +62,11 @@ export default function Step3() {
                 type="checkbox"
                 name="online-service"
                 id="online-service"
+                onChange={(e) => handleInputChange(e)}
               />
               <div
                 className={`pseudo-checkbox ${
-                  onlineServiceState ? "selected" : "not-selected"
+                  onlineService.onlineServiceState ? "selected" : "not-selected"
                 }`}
               ></div>
             </div>
@@ -44,14 +74,84 @@ export default function Step3() {
               <p className="addons-name">Online service</p>
               <p className="addons-descripttion">Access to multiplayer games</p>
             </div>
-            <p className="additional-price-text">+1$/mo</p>
+            <p className="additional-price-text">
+              {planPeriod.periodicState === "Monthly" ? "+1$/mo" : "+12$/yr"}
+            </p>
+          </label>
+          <label
+            className={`select-addons ${
+              largerStorage.largerStorageState ? "selected" : "not-selected"
+            }`}
+            htmlFor="larger-storage"
+          >
+            <div className="addons-check-label">
+              <input
+                type="checkbox"
+                name="larger-storage"
+                id="larger-storage"
+                onChange={(e) => handleInputChange(e)}
+              />
+              <div
+                className={`pseudo-checkbox ${
+                  largerStorage.largerStorageState ? "selected" : "not-selected"
+                }`}
+              ></div>
+            </div>
+            <div className="addons-information">
+              <p className="addons-name">Larger Storage</p>
+              <p className="addons-descripttion">Extra 1TB of cloud save</p>
+            </div>
+            <p className="additional-price-text">
+              {planPeriod.periodicState === "Monthly" ? "+2$/mo" : "+20$/yr"}
+            </p>
+          </label>
+          <label
+            className={`select-addons ${
+              customizableProfile.customizableProfileState
+                ? "selected"
+                : "not-selected"
+            }`}
+            htmlFor="customizable-profile"
+          >
+            <div className="addons-check-label">
+              <input
+                type="checkbox"
+                name="customizable-profile"
+                id="customizable-profile"
+                onChange={(e) => handleInputChange(e)}
+              />
+              <div
+                className={`pseudo-checkbox ${
+                  customizableProfile.customizableProfileState
+                    ? "selected"
+                    : "not-selected"
+                }`}
+              ></div>
+            </div>
+            <div className="addons-information">
+              <p className="addons-name">Customizable profile</p>
+              <p className="addons-descripttion">
+                Custom theme on your profile
+              </p>
+            </div>
+            <p className="additional-price-text">
+              {planPeriod.periodicState === "Monthly" ? "+2$/mo" : "+20$/yr"}
+            </p>
           </label>
         </div>
         <div className="buttons-div">
-          <button className="go-back-button" type="button">
+          <button
+            className="go-back-button"
+            type="button"
+            onClick={() => planStep.setStep(2)}
+          >
             Go Back
           </button>
-          <button className="next-step-button" type="submit">
+          <button
+            className="next-step-button"
+            type="submit"
+            onClick={() => planStep.setStep(4)}
+          >
             Next Step
           </button>
         </div>
