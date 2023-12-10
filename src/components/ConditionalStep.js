0231c3fe3abf3ae96ec9eacaca547a1dda33aef8
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2023-12-10 10:48:37 -0300

    adicionada componente de renderização condicional do steps

---
+++ src/components/ConditionalStep.js
@@ -0,0 +1,27 @@
+import Step1 from "./Step1";
+import Step2 from "./Step2/Step2.js";
+import Step3 from "./Step3.js";
+import Step4 from "./Step4/Step4.js";
+import { useContext } from "react";
+import { StepContext } from "./App.js";
+
+export default function ConditionalStep() {
+  const stepInfo = useContext(StepContext);
+
+  switch (stepInfo.step) {
+    case 1:
+      return <Step1 />;
+
+    case 2:
+      return <Step2 />;
+
+    case 3:
+      return <Step3 />;
+
+    case 4:
+      return <Step4 />;
+
+    default:
+      break;
+  }
+}
